bf65956c9fa67789bce9c8ed2f8b1b063e05a923
DJLionheart <aundario@gmail.com>
2018-04-16 14:21:06 -0600

    update

---
+++ lectureNotes/04-April/0416-HTML-CSS-3/mq-test/custom.js
@@ -0,0 +1,21 @@
+// $('#mobile_menu_icon').click(function(){
+//     ($('body').addClass('menu_open')
+// })
+
+
+// var slideOutMenu = document.getElementsByClassName("mobile_menu_container");
+
+// TweenMax
+//     .to(slideOutMenu, .4, {x: 0})
+//     .to('menu_icon', 0.5, {scale: 1}, 'start+=0.45)_
+
+//This is what it the css animation might look like in Gsav
+
+/*In React 
+
+Right now: onClick: toggle class
+If condition is true, hide it...
+
+
+
+*/ 
